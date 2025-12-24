15b16a6046612d9f840a69509be9755c5f4bc34b
kwame-Owusu <alfredowusu595@gmail.com>
2025-12-24 17:08:24 +0000

    add correct sha 256s for the binaries

--- Formula/sintesi.rb
+++ Formula/sintesi.rb
@@ -6,20 +6,20 @@ class Lista < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/kwame-Owusu/lista/releases/download/v0.1.0/sintesi-darwin-amd64"
-      sha256 "7b6abf39bd8b4c16ce0729ae2b5af488f094e97672d9c353b07620ed8f0f2e74"
+      sha256 "295f0098867fedd4714f6f248195e6d2341228284b110a6163204dc33f69f5aa"
     else
       url "https://github.com/kwame-Owusu/lista/releases/download/v0.1.0/sintesi-darwin-arm64"
-      sha256 "79a261e2c016336ef54ad2b93dda1a155baeffe1d2cdd1ff83ccb8f3e7b1af69"
+      sha256 "23523ed5371319febda44d5edd631c374ea7917937d8a811421b27db24f62703"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/kwame-Owusu/sintesi/releases/download/v0.1.0/sintesi-linux-amd64"
-      sha256 "bce8cafdc6884ca23e1559b605afcbb29428a78e853ed705ad06ce7d2e8fb62e"
+      sha256 "3487aa6b7608a1f67d53cc1400cd27d05e8884a1ba22305266491f5d90f0eaf7"
     else
       url "https://github.com/kwame-Owusu/sintesi/releases/download/v0.1.0/sintesi-linux-arm64"
-      sha256 "5fde84e9f7d0e70b463ffb62845424746fb7dfc9e54fb9cdaf4b152040f6d6c7"
+      sha256 "8a2aae861502e190a2b2272dcf4244804f9a505dcb7181eddc8e48583c052866"
     end
   end
 
